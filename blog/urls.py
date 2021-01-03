00ad2e22217bc3780b8f0ae3a5ebac621a43f791
lesves <lukas.veskrna@gmail.com>
2021-01-03 13:27:57 +0100

    separated base.html and index.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-	path('', views.SectionList.as_view(), name="blog.info"),
+	path('', TemplateView.as_view(template_name="index.html")),
 	path('blog/', views.PostList.as_view(), name="blog.list"),
 	path('blog/post/<int:year>/<int:month>/<slug:slug>/', views.PostDetail.as_view(), name="blog.post"),
 	path('blog/draft/<slug:slug>/', views.DraftDetail.as_view(), name="blog.draft"),
