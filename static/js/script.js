9bb99cfe156d36e1597176509f71415978d8a86b
Petr Sicho <xsicp01@gjk.cz>
2021-01-04 18:50:03 +0100

    update

--- static/js/script.js
+++ static/js/script.js
@@ -26,8 +26,10 @@ function get_scale(val) {
     return Math.pow(val, 1/scaling_factor);
 }
 
-function scroll(location) {
-    window.location = location
+function mobile() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
 }
 
 let chaos_bugs = [];
@@ -38,6 +40,7 @@ class Chaos_Bug {
         this.y = typeof y !== 'undefined' ? y : random(0, chaos_height);
         this.type = typeof type !== 'undefined' ? type : random(0, 4);
         this.img_ref = image_ref[this.type];
+        this.currect_animation_frame = random(0, frames_per_animation);
         this.angle = typeof angle !== 'undefined' ? angle : random(0, 2*Math.PI*1000)/1000;
 
         this.speed = random(7, 11)/10.0;
@@ -56,7 +59,9 @@ class Chaos_Bug {
     draw(canvas, fast=false) {
         canvas.setTransform(scale, 0, 0, scale, this.x, this.y);
         canvas.rotate(this.angle);
-        canvas.drawImage(this.img_ref, -this.img_ref.width / 2, -this.img_ref.height / 2);
+        let img = this.img_ref[Math.floor(this.currect_animation_frame/frames_per_animation)%this.img_ref.length];
+        this.currect_animation_frame++;
+        canvas.drawImage(img, -img.width / 2, -img.height / 2);
 
         if (!fast) {
             this.update();
@@ -181,7 +186,7 @@ class Stream_Bug {
         this.type2 = random(0, 4);
         this.img_ref2 = image_ref[this.type2];
         this.angle = -Math.PI/2;
-
+        this.currect_animation_frame = 0;
         this.velocity = vec2(0, 1);
         SB_spawn_timeout = SB_spawn_cooldown;
 
@@ -190,11 +195,14 @@ class Stream_Bug {
     draw(canvas) {
         canvas.setTransform(scale, 0, 0, scale, width-75, this.y);
         canvas.rotate(this.angle);
-        canvas.drawImage(this.img_ref1, -this.img_ref1.width / 2, -this.img_ref1.height / 2);
+        let img1 = this.img_ref1[Math.floor(this.currect_animation_frame/frames_per_animation)%this.img_ref1.length];
+        canvas.drawImage(img1, -img1.width / 2, -img1.height / 2);
         canvas.setTransform(scale, 0, 0, scale, width-200, this.y);
         canvas.rotate(this.angle);
-        canvas.drawImage(this.img_ref2, -this.img_ref2.width / 2, -this.img_ref2.height / 2);
+        let img2 = this.img_ref2[Math.floor(this.currect_animation_frame/frames_per_animation)%this.img_ref2.length];
+        canvas.drawImage(img2, -img2.width / 2, -img2.height / 2);
 
+        this.currect_animation_frame++;
         this.update();
     }
     update() {
@@ -220,11 +228,11 @@ let height;
 
 let mouse = vec2(null, null);
 
-let bug0 = new Image();
-let bug1 = new Image();
-let bug2 = new Image();
-let bug3 = new Image();
-let bug4 = new Image();
+let bug0 = [new Image(), new Image(), new Image()];
+let bug1 = [new Image(), new Image(), new Image()];
+let bug2 = [new Image(), new Image(), new Image()];
+let bug3 = [new Image(), new Image()];
+let bug4 = [new Image(), new Image(), new Image()];
 
 let border_repulsion_margin;
 let bug_density = 5;
@@ -236,6 +244,7 @@ let extra_bug_count = 0;
 let extra_bug_dying = 0;
 
 let image_ref = [bug0, bug1, bug2, bug3, bug4];
+let frames_per_animation = 10;
 
 //fps counter stuff
 let frames_to_stabilize = 10;
@@ -245,12 +254,22 @@ let too_slow = 0;
 let filterStrength = 20;
 let frameTime = 0, lastLoop = Date.now(), thisLoop;
 
+
 window.onload = function() {
-    bug0.src = static_url + 'img/bg/0.png';
-    bug1.src = static_url + 'img/bg/1.png';
-    bug2.src = static_url + 'img/bg/2.png';
-    bug3.src = static_url + 'img/bg/3.png';
-    bug4.src = static_url + 'img/bg/4.png';
+    bug0[0].src = static_url + 'img/bg/hnusak1.png';
+    bug0[1].src = static_url + 'img/bg/hnusak2.png';
+    bug0[2].src = static_url + 'img/bg/hnusak3.png';
+    bug1[0].src = static_url + 'img/bg/hovnival1.png';
+    bug1[1].src = static_url + 'img/bg/hovnival2.png';
+    bug1[2].src = static_url + 'img/bg/hovnival3.png';
+    bug2[0].src = static_url + 'img/bg/chroust1.png';
+    bug2[1].src = static_url + 'img/bg/chroust2.png';
+    bug2[2].src = static_url + 'img/bg/chroust3.png';
+    bug3[0].src = static_url + 'img/bg/modrmura1.png';
+    bug3[1].src = static_url + 'img/bg/modrmura2.png';
+    bug4[0].src = static_url + 'img/bg/mura1.png';
+    bug4[1].src = static_url + 'img/bg/mura2.png';
+    bug4[2].src = static_url + 'img/bg/mura3.png';
     canvas_element = document.getElementById('bugs');
     width = canvas_element.scrollWidth;
     chaos_height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
@@ -266,6 +285,8 @@ window.onload = function() {
     for (let y=chaos_height; y<height+100; y-=SB_spawn_cooldown*SB_speed)
         new Stream_Bug(y);
     draw();
+    if (mobile())
+        too_slow = 2;
 
     window.addEventListener('mousemove', function(evt) {
         let rect = canvas_element.getBoundingClientRect();
@@ -273,7 +294,7 @@ window.onload = function() {
     });
 };
 
-function draw() {
+function draw(f, forced=false) {
     if (too_slow === 0) {
         canvas.save();
         canvas.fillStyle = '#00324b';
@@ -294,7 +315,8 @@ function draw() {
             too_slow=1;
             frames_elapsed = 0;
         }
-    } else if (too_slow === 1) {
+    } else if (too_slow === 1 || forced) {
+        console.log(forced);
         canvas.save();
         canvas.fillStyle = '#00324b';
         canvas.fillRect(0, 0, width, height);
@@ -334,4 +356,5 @@ window.onresize = function() {
     chaos_bugs = [];
     for (let a=0; a<max_bug_count; a++)
         new Chaos_Bug();
+    draw(0, true)
 };
