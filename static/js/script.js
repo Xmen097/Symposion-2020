03a81364a2a8595b049fd1f77aca582331fa6318
Petr Sicho <xsicp01@gjk.cz>
2021-01-04 19:17:03 +0100

    static

--- static/js/script.js
+++ static/js/script.js
@@ -247,9 +247,9 @@ let image_ref = [bug0, bug1, bug2, bug3, bug4];
 let frames_per_animation = 10;
 
 //fps counter stuff
-let frames_to_stabilize = 5;
+let frames_to_stabilize = 3;
 let frames_elapsed = 0;
-let max_frame_time = 100; // maximum frame_time
+let max_frame_time = 250; // maximum frame_time
 let too_slow = 0;
 let filterStrength = 20;
 let frameTime = 0, lastLoop = Date.now(), thisLoop;
